1360d2e2fc0c4ff111c7289548ac689f89cdce31
Beatriz <beatrizruizpec@gmail.com>
2018-05-08 13:45:37 +0200

    Prueba diccionario

---
+++ static/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var c1 = new Cliente("1", "Orange");
+            var c2 = new Cliente("2", "Movistar");
+            var c3 = new Cliente("3", "Telepizza");
+            
+    
+            Console.WriteLine(Cliente.NombreClase);
+            Console.WriteLine("¿Cuántas referencias hay?: " + Cliente.NumeroReferencias());
+
+            
+        }
+    }
+
+    public class Cliente
+    {
+        public static string NombreClase = "Cliente";
+        private static int contadorReferencias;
+
+        public static int NumeroReferencias()
+        {
+            return contadorReferencias;
+        }
+
+        public string Codigo { get; set;}
+        public string Nombre { get; set;}
+
+        public Cliente(string codigo, string nombre)
+        {
+            contadorReferencias++;
+            Codigo = codigo;
+            Nombre = nombre;
+        }
+    }
+}
