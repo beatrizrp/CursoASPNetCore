f9c68680a89198f4988e6a05db7bb8c753111dc9
Beatriz <beatrizruizpec@gmail.com>
2018-05-09 14:31:58 +0200

    Mi primera API.

---
+++ MiAPI/MiAPI/Controllers/TodoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MiAPI.Models;
+using MiAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Todo")]
+    public class TodoController : Controller
+    {
+        FakeTodoServices _service;
+        public TodoController()
+        {
+            _service = new FakeTodoServices();
+        }
+
+        [HttpGet]
+        public IList<TodoList> GetLists()
+        {
+            return _service.GetTodoLists();
+        }
+    }
+}
---
+++ MiAPI/MiAPI/Models/TodoList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiAPI.Models
+{
+    public class TodoList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Owner { get; set;}
+        public List<TodoItem> Items { get; set; }
+    }
+
+    public class TodoItem
+    {
+        public int TodoListId { get; set; }
+        public string Description { get; set; }
+        public bool Done { get; set; }
+    }
+}
