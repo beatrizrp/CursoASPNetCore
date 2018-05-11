7008fb37c60a10829155743a0c6cff31b4726bc3
Beatriz <beatrizruizpec@gmail.com>
2018-05-11 11:21:00 +0200

    Añadido middleware.

--- MiAPI/MiAPI/Startup.cs
+++ MiAPI/MiAPI/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using MiAPI.Persistence;
@@ -32,19 +33,38 @@ namespace MiAPI
                 options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
                 );
 
+            // Inyección de servicios para la aplicación.
             services.AddTransient<ITodoService, TodoService>();
 
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            // COnfiguración del Pipelinde ASP Net Core
+            loggerFactory
+                .AddEventSourceLogger()
+                .AddConsole();
+
+            var logger = loggerFactory.CreateLogger("Profiler");
+
+            // Configuración del Pipelinde ASP Net Core
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.Use(async (context, next) =>
+            {
+                var watch = Stopwatch.StartNew();
+                await next();
+
+                var path = context.Request.Path;
+                var statusCode = context.Response.StatusCode;
+                var logString = $"Path = '{path}', status = {statusCode}, time = {watch.Elapsed}";
+
+                logger.LogInformation(logString);
+            });
 
             app.UseMvc();
         }
